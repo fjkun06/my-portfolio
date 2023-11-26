61ede8ae94082cc653150d45ac6d4c93d5534008
= <=>
2023-11-26 12:59:11 +0100

    Added icon Components

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,1 +1,7 @@
 export { default as MenuIcon } from "@/components/icons/MenuIcon";
+export { default as GermanyIcon } from "@/components/icons/GermanyIcon";
+export { default as AmericaIcon } from "@/components/icons/AmericaIcon";
+export { default as TwitterIcon } from "@/components/icons/TwitterIcon";
+export { default as GithubIcon } from "@/components/icons/GithubIcon";
+export { default as LinkedInIcon } from "@/components/icons/LinkedInIcon";
+export { default as XingIcon } from "@/components/icons/XingIcon";
